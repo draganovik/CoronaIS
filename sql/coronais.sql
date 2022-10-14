1d73be302a4239506b76ea05b595a806c1b95a64
Mladen Draganović <mladenoneacc@gmail.com>
2022-10-14 14:11:32 +0200

    🔧 Completed filter based on isolation type

--- sql/coronais.sql
+++ sql/coronais.sql
@@ -24,3 +24,21 @@ FROM Patient PA
 LEFT JOIN Hospital HO ON HO.id = PA.hospital_id
 LEFT JOIN CoronaHotspotRegion CHR on CHR.region_code = PA.departure_region_code
 WHERE PA.state_issued_id = 5002300672519
+
+SELECT
+PA.state_issued_id,
+PA.start_of_isolation,
+PA.full_name,
+PA.departure_region_code,
+CHR.region_name,
+PA.has_symptoms,
+PA.phone_number,
+PA.hospital_id,
+HO.max_capacity,
+HO.facility_name,
+PA.citizenship,
+CHR.region_code
+FROM Patient PA
+LEFT JOIN Hospital HO ON HO.id = PA.hospital_id
+LEFT JOIN CoronaHotspotRegion CHR on CHR.region_code = PA.departure_region_code
+WHERE 1=1 AND CHR.region_code IS NOT NULL
