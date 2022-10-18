7d7fb2f324c41b2f9ac6212bff87488adf11e0ae
Mladen Draganović <mladenoneacc@gmail.com>
2022-10-18 14:18:16 +0200

    ✏️ New error format + assignHospital bug fixed

--- sql/coronais.sql
+++ sql/coronais.sql
@@ -24,24 +24,7 @@ FROM Patient PA
 LEFT JOIN Hospital HO ON HO.id = PA.hospital_id
 LEFT JOIN CoronaHotspotRegion CHR on CHR.region_code = PA.departure_region_code
 WHERE PA.state_issued_id = 5002300672519;
-
-SELECT
-PA.state_issued_id,
-PA.start_of_isolation,
-PA.full_name,
-PA.departure_region_code,
-CHR.region_name,
-PA.has_symptoms,
-PA.phone_number,
-PA.hospital_id,
-HO.max_capacity,
-HO.facility_name,
-PA.citizenship,
-CHR.region_code
-FROM Patient PA
-LEFT JOIN Hospital HO ON HO.id = PA.hospital_id
-LEFT JOIN CoronaHotspotRegion CHR on CHR.region_code = PA.departure_region_code
-WHERE 1=1 AND CHR.region_code IS NOT NULL;
+/*WHERE 1=1 AND CHR.region_code IS NOT NULL;*/
 
 
 SELECT *
@@ -93,10 +76,9 @@ WHERE has_symptoms = 1;
 
 UPDATE Patient SET start_of_isolation = '2022-10-16' WHERE full_name = 'Mirko Marko';
 
-
 SELECT id, max_capacity, facility_name, (max_capacity - COUNT(PA.state_issued_id)) as 'available_space'
 FROM Hospital HO
-JOIN Patient PA ON PA.hospital_id = HO.id
+LEFT JOIN Patient PA ON PA.hospital_id = HO.id
 GROUP BY id, max_capacity, facility_name
 ORDER BY available_space desc
-LIMIT 1
+LIMIT 1;
